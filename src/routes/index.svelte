cf97877a210d2e50db0c03641e73cb122580e64f
JesseKuntz <jessekuntz96@gmail.com>
2022-08-26 21:43:54 -0400

    feat: simplify header banner

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,68 +4,71 @@
 
 <main class="center-everything">
 	<div class="header-container">
+		<div class="wave-container">
+			<svg viewBox="0 0 1440 160" xmlns="http://www.w3.org/2000/svg">
+				<defs>
+					<style type="text/css">
+						.wave {
+							animation: wave 4s linear infinite;
+						}
+
+						.wave1 {
+							animation: wave1 5s linear infinite;
+						}
+
+						.wave2 {
+							animation: wave2 6s linear infinite;
+						}
+
+						@keyframes wave {
+							0% {
+								transform: translateX(0%);
+							}
+
+							100% {
+								transform: translateX(100%);
+							}
+						}
+
+						@keyframes wave1 {
+							0% {
+								transform: scaleY(1.2) translateX(0%);
+							}
+
+							100% {
+								transform: scaleY(1.2) translateX(100%);
+							}
+						}
+
+						@keyframes wave2 {
+							0% {
+								transform: scaleY(0.8) translateX(0%);
+							}
+
+							100% {
+								transform: scaleY(0.8) translateX(100%);
+							}
+						}
+					</style>
+					<path
+						id="sine-wave"
+						fill="#5b1717"
+						fill-opacity="0.6"
+						d="M0,80 C320,150,420,150,740,80 C1060,0,1120,0,1440,80 V0 H0"
+					/>
+				</defs>
+				<use class="wave" href="#sine-wave" />
+				<use class="wave" x="-100%" href="#sine-wave" />
+				<use class="wave1" href="#sine-wave" />
+				<use class="wave1" x="-100%" href="#sine-wave" />
+				<use class="wave2" href="#sine-wave" />
+				<use class="wave2" x="-100%" href="#sine-wave" />
+			</svg>
+		</div>
 		<h1>Shrekify</h1>
 		<div class="header-background" />
 	</div>
-	<svg viewBox="0 0 1440 160" xmlns="http://www.w3.org/2000/svg">
-		<defs>
-			<style type="text/css">
-				.wave {
-					animation: wave 4s linear infinite;
-				}
 
-				.wave1 {
-					animation: wave1 5s linear infinite;
-				}
-
-				.wave2 {
-					animation: wave2 6s linear infinite;
-				}
-
-				@keyframes wave {
-					0% {
-						transform: translateX(0%);
-					}
-
-					100% {
-						transform: translateX(100%);
-					}
-				}
-
-				@keyframes wave1 {
-					0% {
-						transform: scaleY(1.2) translateX(0%);
-					}
-
-					100% {
-						transform: scaleY(1.2) translateX(100%);
-					}
-				}
-
-				@keyframes wave2 {
-					0% {
-						transform: scaleY(0.8) translateX(0%);
-					}
-
-					100% {
-						transform: scaleY(0.8) translateX(100%);
-					}
-				}
-			</style>
-			<path
-				id="sine-wave"
-				fill="#5b1717"
-				fill-opacity="0.6"
-				d="M0,80 C320,150,420,150,740,80 C1060,0,1120,0,1440,80 V0 H0"
-			/>
-		</defs>
-		<use class="wave" href="#sine-wave" />
-		<use class="wave" x="-100%" href="#sine-wave" />
-		<use class="wave1" href="#sine-wave" />
-		<use class="wave1" x="-100%" href="#sine-wave" />
-		<use class="wave2" href="#sine-wave" />
-		<use class="wave2" x="-100%" href="#sine-wave" />
-	</svg>
 
 	<DropArea />
 </main>
@@ -84,8 +87,14 @@
 		font-size: 26px;
 	}
 
+	.wave-container {
+		position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 0;
+	}
+
 	.header-container {
-		width: 100%;
 		position: relative;
 	}
 
@@ -97,6 +106,7 @@
 		width: 100%;
 		height: 100%;
 		z-index: -1;
+		border-radius: 0 0 20px 20px;
 	}
 
 	h1 {
@@ -105,8 +115,10 @@
 		background: linear-gradient(lime, green);
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
-		padding: 0px 4px;
-		margin: 30px 0;
+		margin: 0;
+		padding: 20px 40px;
+		z-index: 1;
+    position: relative;
 	}
 
 	.center-everything {
@@ -119,6 +131,7 @@
 	@media (max-width: 600px) {
 		h1 {
 			font-size: 60px;
+			padding: 20px 20px;
 		}
 	}
 </style>
