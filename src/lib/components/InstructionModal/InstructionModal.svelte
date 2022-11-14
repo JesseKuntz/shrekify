84fdd766b8b55cacb53a89bebfc7bb23ac8baa78
JesseKuntz <jessekuntz96@gmail.com>
2022-11-13 21:25:34 -0500

    feat: style the modal

--- src/lib/components/InstructionModal/InstructionModal.svelte
+++ src/lib/components/InstructionModal/InstructionModal.svelte
@@ -8,6 +8,25 @@
 	const showModal = () => modal.set(bind(Content, {}));
 </script>
 
-<Modal show={$modal}>
+<Modal
+	show={$modal}
+	styleContent={{
+		border: '4px solid #3f2a1f',
+		borderRadius: '8px',
+		background: 'linear-gradient(#dbb098, #67985d)',
+		maxHeight: 'calc(100vh - 32px)',
+		textAlign: 'left'
+	}}
+	styleCloseButton={{
+		right: '32px'
+	}}
+	styleWindow={{
+		width: '800px',
+		margin: '16px auto'
+	}}
+	styleWindowWrap={{
+		margin: '0 16px'
+	}}
+>
 	<Button color={'40'} onClick={showModal} icon="info">Instructions</Button>
 </Modal>
